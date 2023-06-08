f1492313b11373f27ed2adab68d97d4715bd5e64
nonkululeko <nonkululekon@wsb.co.za>
2023-06-08 16:46:09 +0200

    first commit

---
+++ src/Logo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+export default function Logo() {
+  return (
+    <>
+    <div className='logo-holder'>
+        <div className='logo'>
+        <i className="ri-mickey-fill"></i>
+        </div>
+    </div>
+    <div className='title'>Minion</div>
+    </>
+  )
+}
