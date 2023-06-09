d1915ffe6fbd3bd6d0f0af0dd11ccaec984ecc6e
nonkululeko <nonkululekon@wsb.co.za>
2023-06-09 08:32:55 +0200

    inserted logo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Logo from './Logo'
 import Form from './Form'
-import { RiMickeyFill } from 'react-icons/ri';
+
 
 const App = () => {
   return (
     <div>
       <Logo/>
       <Form/>
-      <RiMickeyFill />
+ {/*      <RiMickeyFill /> */}
     </div>
   )
 }
